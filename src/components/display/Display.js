ffdf373af4dc69bad801aa4fa80d144eab605620
JashanB <jashan_brar50@hotmail.com>
2020-10-18 10:18:52 -0400

    used prop types to debug

--- src/components/display/Display.js
+++ src/components/display/Display.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Display.css';
+// import PropTypes from 'prop-types'
+
 
 export default function Display (props) {
   const newNum = props.displayNum / 2
@@ -9,3 +11,7 @@ export default function Display (props) {
     </>
   )
 }
+
+// Display.propTypes = {
+//   displayNum: PropTypes.number
+// }
